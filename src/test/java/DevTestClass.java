56a109fedf662928daa816229ed80a3e1d3f0af1
Mir Al Mamun <miraalmamun@gmail.com>
2022-10-19 18:16:04 -0400

    Modify DevTestClass Added new functionality

--- src/test/java/DevTestClass.java
+++ src/test/java/DevTestClass.java
@@ -2,5 +2,6 @@ public class DevTestClass {
 
     public static void main(String[] args) {
         System.out.println("DevTestClass");
+        System.out.println("Second change for pushing purpose");
     }
 }
